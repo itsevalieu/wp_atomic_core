db277eebf17cf26cc26fe9a75945f041c52aa7c8
Eva Lieu <itsevalieu@gmail.com>
2021-03-28 21:37:43 -0700

    Disable default dashboard widgets on the admin

--- functions.php
+++ functions.php
@@ -33,3 +33,19 @@ foreach ( $understrap_includes as $file ) {
 	}
 	require_once $filepath;
 }
+
+//Disable Default Dashboard Widgets
+function remove_dashboard_meta() {
+    remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal'); //Removes the 'incoming links' widget
+    remove_meta_box('dashboard_plugins', 'dashboard', 'normal'); //Removes the 'plugins' widget
+    remove_meta_box('dashboard_primary', 'dashboard', 'normal'); //Removes the 'WordPress News' widget
+    remove_meta_box('dashboard_secondary', 'dashboard', 'normal'); //Removes the secondary widget
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'side'); //Removes the 'Quick Draft' widget
+    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side'); //Removes the 'Recent Drafts' widget
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal'); //Removes the 'Activity' widget
+    remove_meta_box('dashboard_right_now', 'dashboard', 'normal'); //Removes the 'At a Glance' widget
+    remove_meta_box('dashboard_activity', 'dashboard', 'normal'); //Removes the 'Activity' widget (since 3.8)
+    remove_meta_box('rg_forms_dashboard', 'dashboard', 'normal'); //Removes the 'Activity' widget (since 3.8)
+    remove_action('admin_notices', 'update_nag');
+}
+add_action('admin_init', 'remove_dashboard_meta');
