f064bfbd73e2e47845473ee4e53ce33b8978bbcf
Eva Lieu <itsevalieu@gmail.com>
2021-03-28 21:45:22 -0700

    Moved disable dashboard widgets to its own function file

---
+++ admin-dashboard/dashboard-widgets.php
@@ -0,0 +1,21 @@
+<?php
+/**
+*
+* Disable Default Dashboard Widgets
+*
+**/
+function remove_dashboard_meta() {
+    remove_meta_box('dashboard_incoming_links', 'dashboard', 'normal'); //Removes the 'incoming links' widget
+    remove_meta_box('dashboard_plugins', 'dashboard', 'normal'); //Removes the 'plugins' widget
+    remove_meta_box('dashboard_primary', 'dashboard', 'normal'); //Removes the 'WordPress News' widget
+    remove_meta_box('dashboard_secondary', 'dashboard', 'normal'); //Removes the secondary widget
+    remove_meta_box('dashboard_quick_press', 'dashboard', 'side'); //Removes the 'Quick Draft' widget
+    remove_meta_box('dashboard_recent_drafts', 'dashboard', 'side'); //Removes the 'Recent Drafts' widget
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal'); //Removes the 'Activity' widget
+    remove_meta_box('dashboard_right_now', 'dashboard', 'normal'); //Removes the 'At a Glance' widget
+    remove_meta_box('dashboard_activity', 'dashboard', 'normal'); //Removes the 'Activity' widget (since 3.8)
+    remove_meta_box('rg_forms_dashboard', 'dashboard', 'normal'); //Removes the 'Activity' widget (since 3.8)
+    remove_action('admin_notices', 'update_nag');
+}
+add_action('admin_init', 'remove_dashboard_meta');
+?>
